84045a041668579f0d4b82f2fc0e4b1157291b06
Abhishek-raj093 <ss0986825@gmail.com>
2025-09-16 10:45:03 +0530

    Created the API to generate text to image and connected to the Cloudinary

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,8 +1,8 @@
-
-
 import OpenAI from "openai";
 import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
+import {v2 as cloudinary} from 'cloudinary'
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -36,7 +36,7 @@ export const generateArticle = async (req, res) => {
 
         await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'article')`;
 
-        if(plan !== 'premium'){
+        if (plan !== 'premium') {
             await clerkClient.users.updateUserMetadata(userId, {
                 privateMetadata: {
                     free_usage: free_usage + 1
@@ -44,11 +44,11 @@ export const generateArticle = async (req, res) => {
             })
         }
 
-        res.json({success: true, content})
+        res.json({ success: true, content })
 
     } catch (error) {
         console.log(error.message)
-        res.json({success:false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
@@ -79,7 +79,7 @@ export const generateBlogTitle = async (req, res) => {
 
         await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
 
-        if(plan !== 'premium'){
+        if (plan !== 'premium') {
             await clerkClient.users.updateUserMetadata(userId, {
                 privateMetadata: {
                     free_usage: free_usage + 1
@@ -87,10 +87,40 @@ export const generateBlogTitle = async (req, res) => {
             })
         }
 
-        res.json({success: true, content})
+        res.json({ success: true, content })
 
     } catch (error) {
         console.log(error.message)
-        res.json({success:false, message: error.message})
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export const generateImage = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { prompt, publish } = req.body;
+        const plan = req.plan;
+
+        if (plan !== 'premium') {
+            return res.json({ success: false, message: 'This feature is only available for premium subscriptions' })
+        }
+
+        const formData = new FormData()
+        formData.append('prompt', prompt)
+        const {data} = await axios.post('https://clipdrop-api.co/text-to-image/v1', formData, {headers: {'x-api-key': process.env.CLIPDROP_API_KEY},
+         responseType: 'arraybuffer',
+        })
+
+        const base64Image = `data:image/png;base64,${Buffer.from(data, 'binary').toString('base64')}`;
+
+        const {secure_url} = await cloudinary.uploader.upload(base64Image)
+
+        await sql`INSERT INTO creations (user_id, prompt, content, type, publish) VALUES (${userId}, ${prompt}, ${secure_url}, 'image', ${publish ?? false})`;
+
+        res.json({ success: true, content: secure_url })
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
     }
 }
