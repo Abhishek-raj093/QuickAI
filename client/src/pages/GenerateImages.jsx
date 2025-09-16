6c136435a566ee815147f63c4f15a533c59ae79a
Abhishek-raj093 <ss0986825@gmail.com>
2025-09-16 21:32:44 +0530

    Project Completed

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -1,16 +1,43 @@
 import { Image, Sparkles } from 'lucide-react';
 import React, { useState } from 'react'
+import axios from 'axios'
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const GenerateImages = () => {
 
     const imageStyle = ['Realistic', 'Ghibli style', 'Anime style', 'Cartoon style', 'Fantasy style', 'Realistic style', '3D style', 'Portrait style']
 
-    const [slectedStyle, setSelectedStyle] = useState('Realistic');
+    const [selectedStyle, setSelectedStyle] = useState('Realistic');
     const [input, setInput] = useState('')
     const [publish, setPublish] = useState(false)
 
+    const [loading, setLoading] = useState(false);
+    const [content, setContent] = useState('');
+
+    const { getToken } = useAuth();
+
     const onSubmitHandler = async (e) => {
         e.preventDefault();
+        try {
+            setLoading(true)
+
+            const prompt = `Generate an image of ${input} in the style ${selectedStyle}`
+
+            const { data } = await axios.post('/api/ai/generate-image', { prompt, publish },
+                { headers: { Authorization: `Bearer ${await getToken()}` } })
+
+            if (data.success) {
+                setContent(data.content)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+        setLoading(false)
     }
 
     return (
@@ -29,7 +56,7 @@ const GenerateImages = () => {
 
                 <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
                     {imageStyle.map((item) => (
-                        <span onClick={() => setSelectedStyle(item)} className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${slectedStyle === item ? 'bg-green-50 text-green-700' : 'text-gray-500 border-gray-300'}`} key={item}>{item}</span>
+                        <span onClick={() => setSelectedStyle(item)} className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedStyle === item ? 'bg-green-50 text-green-700' : 'text-gray-500 border-gray-300'}`} key={item}>{item}</span>
                     ))}
                 </div>
 
@@ -44,23 +71,36 @@ const GenerateImages = () => {
                     <p className='text-sm'>Make this image Public</p>
                 </div>
 
-                <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-                    <Image className='w-5' />
+                <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+                    {
+                        loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span>
+                            : <Image className='w-5' />
+                    }
                     Generate Image
                 </button>
             </form>
             {/* Right Col */}
-            <div className='w-fullmax-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
+            <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
                 <div className='flex items-center gap-3'>
                     <Image className='w-5 h-5 text-[#00AD25]' />
                     <h1 className='text-xl font-semibold'>Generated image</h1>
                 </div>
-                <div className='flex-1 flex justify-center items-center'>
-                    <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-                        <Image className='w-9 h-9' />
-                        <p>Enter a topic and click "Generate image" to get started</p>
-                    </div>
-                </div>
+
+                {
+                    !content ? (
+                        <div className='flex-1 flex justify-center items-center'>
+                            <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+                                <Image className='w-9 h-9' />
+                                <p>Enter a topic and click "Generate image" to get started</p>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className='mt-3 h-full'>
+                            <img src={content} alt="image" className='w-full h-full' />
+                        </div>
+                    )
+                }
+
             </div>
         </div>
     )
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,7 @@ export const getUserCreations = async (req, res)=>{
     try {
         const {userId} = req.auth()
 
-        const creations = await sql`SELECT * FROM creations WHERE user_id = ${userId} ORDER BY created_at DESC`;
+        const creations = await sql`SELECT * FROM creations WHERE user_id = ${userId} ORDER BY crested_at DESC`;
 
         res.json({ success: true, creations })
     } catch (error) {
@@ -16,7 +16,7 @@ export const getUserCreations = async (req, res)=>{
 export const getPublishedCreations = async (req, res)=>{
     try {
 
-        const creations = await sql`SELECT * FROM creations WHERE publish = true ORDER BY created_at DESC`;
+        const creations = await sql`SELECT * FROM creations WHERE publish = true ORDER BY crested_at DESC`;
 
         res.json({ success: true, creations })
     } catch (error) {
@@ -30,7 +30,7 @@ export const toggleLikeCreation = async (req, res)=>{
         const {userId} = req.auth()
         const {id} = req.body
 
-        const [creation] = await sql`SELECT * FROM crestioins WHERE id = ${id}`
+        const [creation] = await sql`SELECT * FROM creations WHERE id = ${id}`
 
         if (!creation) {
             return res.json({ success: false, message: "Creation not found" })
@@ -49,7 +49,7 @@ export const toggleLikeCreation = async (req, res)=>{
             message = 'Creation Liked'
         }
 
-        const formattedArray = `{${updatedLikes.json(',')}}`
+        const formattedArray = `{${updatedLikes.join(',')}}`
 
         await sql`UPDATE creations SET likes = ${formattedArray}::text[] WHERE id = ${id}`;
 
